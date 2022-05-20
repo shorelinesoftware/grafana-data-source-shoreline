dd6cfc2605ef60a579b0c2775064193e67f236ff
bscheue-shoreline <brian@shoreline.io>
2022-05-19 17:41:01 -0700

    Handle no relevant data in op result

--- src/datasource.ts
+++ src/datasource.ts
@@ -151,6 +151,7 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
         if ('linux_cmd' in response) {
           return this.buildLinuxCmdDataFrames(response, query.refId, resourceIdToName);
         }
+        return [];
       });
     });
 
